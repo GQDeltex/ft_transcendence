6ef5d20d8aa9c4d021d9d60500171c3979a8d0e6
hennneh <hendrik.lehmann96@gmail.com>
2022-09-21 10:35:43 +0200

    linter

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 //import { CustomersModule } from './customers/customers.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import {TypeOrmModule} from '@nestjs/typeorm';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -23,7 +22,7 @@ import {TypeOrmModule} from '@nestjs/typeorm';
       inject: [ConfigService],
     }),
     UsersModule,
-   ],
+  ],
   controllers: [],
   providers: [],
 })
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -4,6 +4,6 @@ import { UsersService } from './services/users/users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
