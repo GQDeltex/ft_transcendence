fac623a7aa47b17eae1c342025a458e622285ffd
hennneh <hendrik.lehmann96@gmail.com>
2022-09-21 10:31:21 +0200

    setting up db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+//import { CustomersModule } from './customers/customers.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ backend/src/users/users.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './controllers/users/users.controller';
+import { UsersService } from './services/users/users.service';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService]
+})
+export class UsersModule {}
