16dec896ab860e2968b55a50adac4002e349a45f
Hendrik Lehmann <hendrik.lehmann96@gmail.com>
2022-09-23 15:26:38 +0200

    starting on the landing page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,91 +1,75 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+    <div>
+      <!--  <img
+		  alt="page logo"
+		  class="logo"
+		  src="@/assets/logo.svg"
+		  width="125"
+		  height="125"
+      /> add the LOGO later here!!!!!!!!!!!!!!!!!!!!!!please!!!!!!!!!!!!!!!! -->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>trans_logo</h1>
+      <!-- just a placeholder!!!!!!!11elf -->
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <h2>username-placeholder</h2>
+
+      <!--  <img
+		  alt="page logo"
+		  class="logo"
+		  src="@/assets/logo.svg"
+		  width="125"
+		  height="125"
+      /> add the LOGO later here!!!!!!!!!!!!!!!!!!!!!!please!!!!!!!!!!!!!!!! -->
     </div>
+    <nav>
+      <li><RouterLink to="leaderboard">Leaderboard</RouterLink></li>
+      <li><RouterLink to="play">Play Now</RouterLink></li>
+      <li><RouterLink to="skin">Skin Selection</RouterLink></li>
+      <li><RouterLink to="stream">Stream</RouterLink></li>
+      <li><RouterLink to="chat">Chat</RouterLink></li>
+    </nav>
   </header>
-
   <RouterView />
 </template>
 
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+  background-color: black;
+}
+</style>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+h2 {
+  float: right;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
-  width: 100%;
-  font-size: 12px;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
+li {
+  display: inline;
+  flex-grow: 4;
+}
+li a {
+  display: block;
+  color: #f8971d;
   text-align: center;
-  margin-top: 2rem;
+  padding: 14px 16px;
+  text-decoration: none;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+li a:hover {
+  background-color: #f8971d;
+  color: black;
 }
 </style>
