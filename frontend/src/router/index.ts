451484b3adb0dfbf2bd70134f93bf53a72db31a1
Hendrik Lehmann <70098821+hennneh@users.noreply.github.com>
2022-09-26 12:07:23 +0200

    404 page done and added to router (#23)

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LandingView from '../views/LandingView.vue';
+import PageNotFoundView from '../views/PageNotFoundView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/:catchAll(.*)*',
+      name: 'PageNotFoundView',
+      component: PageNotFoundView,
     },
   ],
 });
---
+++ frontend/src/views/PageNotFoundView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>ERROR 404: PAGE NOT FOUND</h1>
+  </main>
+</template>
