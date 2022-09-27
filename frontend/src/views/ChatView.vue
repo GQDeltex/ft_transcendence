6e2a8015122990a65d5912419dab551da991eaac
Peter Stengl <pstengl@student.42wolfsburg.de>
2022-09-27 17:43:11 +0200

    Add Websocket test code

---
+++ frontend/src/views/ChatView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:8080');
+
+socket.on('connect', function () {
+  console.log('Connected');
+  socket.emit('events', { test: 'test' });
+  socket.emit('identity', 0, (response) => console.log('Identity:', response));
+});
+
+socket.on('events', function (data) {
+  console.log('event', data);
+});
+
+socket.on('exception', function (data) {
+  console.log('event', data);
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected');
+});
+</script>
+
+<template>
+  <h1>Chat View (PRC)</h1>
+</template>
+
+<style scoped></style>
