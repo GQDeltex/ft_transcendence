7c86fa0ab0c381be729a40a7f2b89ee9f8dde068
Hendrik Lehmann <70098821+hennneh@users.noreply.github.com>
2022-09-27 16:02:13 +0200

    28 leaderboard page (#41)
    * basic layout done for leaderboard
    
    * testing out gold-pongking-boi on leaderboard
    
    * basic leaderboard done
    
    Co-authored-by: Clemens Dahlhoff <78210806+c8p2d01@users.noreply.github.com>

---
+++ frontend/src/components/LeaderboardCategoryComponent.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import LeaderboardPlayerComponent from './LeaderboardPlayerComponent.vue';
+//import LeaderboardMedalComponent from './LeaderboardMedalComponent.vue';
+defineProps<{
+  categoryName: string;
+}>();
+</script>
+
+<template>
+  <h3>{{ categoryName }}</h3>
+  <div class="categories">
+    <!--    <LeaderboardMedalComponent medal-name="gucalvi" /> -->
+    <LeaderboardPlayerComponent player-name="gucalvi" />
+    <LeaderboardPlayerComponent player-name="gucalvi" />
+    <LeaderboardPlayerComponent player-name="gucalvi" />
+    <LeaderboardPlayerComponent player-name="gucalvi" />
+    <LeaderboardPlayerComponent player-name="gucalvi" />
+  </div>
+</template>
+
+<style scoped>
+h3 {
+  padding: 5px;
+  color: orange;
+}
+.categories {
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+.gold {
+  position: relative;
+  bottom: 70px;
+}
+</style>
