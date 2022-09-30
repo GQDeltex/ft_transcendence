cefd534a8c369f0cbc25487fe8f9fd1d189b6d84
GQDeltex <pstengl@student.42wolfsburg.de>
2022-09-30 13:41:02 +0200

    Update users to GraphQL (#59)
    * Update users to GraphQL
    
    * Change Folder structure (again...)

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  create(createUserInput: CreateUserInput) {
+    const newUser = this.userRepository.create(createUserInput);
+    return this.userRepository.upsert(newUser, ['id']);
+  }
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  findOne(identifier: number | string) {
+    if (typeof identifier == 'number')
+      return this.userRepository.findOneBy({ id: identifier });
+    else return this.userRepository.findOneBy({ username: identifier });
+  }
+
+  async updatePicture(id: number, picture: string) {
+    await this.userRepository.update(id, { picture: picture });
+    return this.findOne(id);
+  }
+
+  async updateUsername(id: number, username: string) {
+    await this.userRepository.update(id, { username: username });
+    return this.findOne(id);
+  }
+}
