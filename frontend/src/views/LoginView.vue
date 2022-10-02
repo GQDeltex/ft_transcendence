7bd1d8883050724f44709fb9626d0cd0d0c11cd5
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-10-03 01:07:05 +0200

    lint LoginView

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,13 +1,13 @@
 <template>
-    <a href="http://localhost:8080/login">Login</a>
+  <a href="http://localhost:8080/login"> Login </a>
 </template>
 
 <style scoped>
 a {
-    text-decoration: none;
-    radius: 5px;
-    color: black;
-    background-color: white;
-    padding: 10px;
+  text-decoration: none;
+  border-radius: 5px;
+  color: black;
+  background-color: white;
+  padding: 10px;
 }
 </style>
