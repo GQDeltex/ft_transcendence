a4ab89fa3fe2595307ec1144c6b425abb087b36e
Konstantin Meixner <konstantin.meixner@freenet.de>
2022-10-07 20:31:26 +0200

    added skeleton for stream page with categories

---
+++ frontend/src/components/StreamGameComponent.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  player1Name: string;
+  player2Name: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <img class="thumbnail" src="@/assets/pong.png" width="113" height="70" />
+    <span class="playernames"> {{ player1Name }} vs {{ player2Name }}</span>
+  </div>
+</template>
+<style scoped>
+div {
+  display: grid;
+  margin: auto;
+}
+.playernames {
+  display: flex;
+  justify-content: center;
+  color: white;
+  text-decoration: none;
+}
+.thumbnail {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ frontend/src/views/StreamView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import StreamCategoryComponent from '../components/StreamCategoryComponent.vue';
+</script>
+
+<template>
+  <StreamCategoryComponent category-name="Top Rated" />
+  <StreamCategoryComponent category-name="Most Popular" />
+</template>
+
+<style scoped></style>
