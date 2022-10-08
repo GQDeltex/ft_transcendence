f70797d91b565ffed65ed0baf36e6f6f0bf8bd54
Konstantin Meixner <konstantin.meixner@freenet.de>
2022-10-08 10:50:11 +0200

    added template for search component

--- frontend/src/views/StreamView.vue
+++ frontend/src/views/StreamView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import StreamCategoryComponent from '../components/StreamCategoryComponent.vue';
+import SearchComponent from '../components/SearchComponent.vue';
 </script>
 
 <template>
-  <StreamCategoryComponent category-name="Top Rated" />
-  <StreamCategoryComponent category-name="Most Popular" />
+  <div>
+    <SearchComponent />
+  </div>
+  <div>
+    <StreamCategoryComponent category-name="Top Rated" />
+    <StreamCategoryComponent category-name="Most Popular" />
+  </div>
 </template>
 
 <style scoped></style>
