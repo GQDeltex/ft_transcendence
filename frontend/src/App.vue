00fddb471dd9f0c39f551a07d733e02f53bb65a1
kmeixner247 <81682578+kmeixner247@users.noreply.github.com>
2022-10-08 14:23:36 +0200

    Navbar into component (#92)
    * moved navbar form App.vue into its own component
    
    * Ran Prettier
    
    Co-authored-by: Vincent Heymans <belgishboy@gmail.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,41 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
 import { useUserStore } from './store/user';
+import NavbarComponent from './components/NavbarComponent.vue';
 
 const userStore = useUserStore();
 </script>
 
 <template>
   <header v-if="userStore.isLoggedIn">
-    <div class="column1">
-      <router-link to="/" class="columncontent">
-        <img
-          alt="page logo"
-          class="logo"
-          src="@/assets/pongking_boi.svg"
-          height="50"
-        />
-        <span>pongking</span>
-      </router-link>
-    </div>
-    <div class="column2">
-      <router-link to="/profile" class="columncontent">
-        <span>{{ userStore.username }}</span>
-        <img
-          alt="page logo"
-          class="logo"
-          :src="userStore.picture"
-          height="50"
-        />
-      </router-link>
-    </div>
-    <nav>
-      <li><RouterLink to="leaderboard">Leaderboard</RouterLink></li>
-      <li><RouterLink to="play">Play Now</RouterLink></li>
-      <li><RouterLink to="skin">Skin Selection</RouterLink></li>
-      <li><RouterLink to="stream">Stream</RouterLink></li>
-      <li><RouterLink to="chat">Chat</RouterLink></li>
-    </nav>
+    <NavbarComponent />
   </header>
   <RouterView />
 </template>
@@ -52,51 +25,5 @@ body {
 header {
   display: grid;
   row-gap: 10px;
-}
-img {
-  margin-left: 15px;
-}
-.column1 {
-  grid-column: 1 / 2;
-  justify-self: start;
-}
-.column2 {
-  grid-column: 2 / 3;
-  justify-self: end;
-}
-.columncontent {
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  color: white;
-}
-nav {
-  border-top: 1px solid grey;
-  border-bottom: 1px solid gray;
-  grid-column: 1 / 3;
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-}
-.router-link-active {
-  color: white;
-}
-li {
-  display: inline;
-  flex-grow: 4;
-}
-li a {
-  display: block;
-  color: #f8971d;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-li a:hover {
-  background-color: #f8971d;
-  color: black;
 }
 </style>
---
+++ frontend/src/components/NavbarComponent.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import { useUserStore } from '../store/user';
+const userStore = useUserStore();
+</script>
+
+<template>
+  <div class="column1">
+    <router-link to="/" class="columncontent">
+      <img
+        alt="page logo"
+        class="logo"
+        src="@/assets/pongking_boi.svg"
+        height="50"
+      />
+      <span>pongking</span>
+    </router-link>
+  </div>
+  <div class="column2">
+    <router-link to="/profile" class="columncontent">
+      <span>{{ userStore.username }}</span>
+      <img alt="page logo" class="logo" :src="userStore.picture" height="50" />
+    </router-link>
+  </div>
+  <nav>
+    <li><RouterLink to="leaderboard">Leaderboard</RouterLink></li>
+    <li><RouterLink to="play">Play Now</RouterLink></li>
+    <li><RouterLink to="skin">Skin Selection</RouterLink></li>
+    <li><RouterLink to="stream">Stream</RouterLink></li>
+    <li><RouterLink to="chat">Chat</RouterLink></li>
+  </nav>
+</template>
+
+<style scoped>
+img {
+  margin-left: 15px;
+}
+.column1 {
+  grid-column: 1 / 2;
+  justify-self: start;
+}
+.column2 {
+  grid-column: 2 / 3;
+  justify-self: end;
+}
+.columncontent {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: white;
+}
+nav {
+  border-top: 1px solid grey;
+  border-bottom: 1px solid gray;
+  grid-column: 1 / 3;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
+.router-link-active {
+  color: white;
+}
+li {
+  display: inline;
+  flex-grow: 4;
+}
+li a {
+  display: block;
+  color: #f8971d;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+li a:hover {
+  background-color: #f8971d;
+  color: black;
+}
+</style>
