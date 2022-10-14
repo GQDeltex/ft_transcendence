be1e1ef59db3257df1f44f6eecdeb71e1002da7f
René Kaufmann <104884267+Kromolux@users.noreply.github.com>
2022-10-14 20:26:28 +0200

    32 user profile page (#115)
    * adding non-functional history component
    
    * fixed a bug in UserHistoryComponent and implemented ProfileComponent
    
    * implemented AboutMeComponent and AchiementComponent - improved loginView
    
    * small changes in loginview and achievements
    
    * historyComponent and achievementsComponent fixed
    
    * Fix styles to be more silimar on Chrome and Firefox
    
    * Fix scaling on fullscreen, update font-size and make profile picture more beautiful
    
    * Fix style
    
    * Simplify ProfileView
    
    Co-authored-by: Hendrik Lehmann <hendrik.lehmann96@gmail.com>
    Co-authored-by: Peter Stengl <pstengl@student.42wolfsburg.de>

---
+++ frontend/src/components/AchievementComponent.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="achievements">
+    <span class="text">Achievements (1)</span>
+    <div class="achieve">
+      <img
+        alt="page logo"
+        class="logo"
+        src="@/assets/pongking_boi.svg"
+        height="40"
+        width="50"
+      />
+      <p class="infoBox">
+        Smol PongKing: <span class="info">first login</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.achievements {
+  display: grid;
+  padding: 10px;
+  border: 1px solid grey;
+  overflow-y: scroll;
+}
+.text {
+  font-size: 0.7em;
+  grid-column: 1 / 6;
+}
+.achieve {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  grid-column: 1 / 2;
+  padding: 10px;
+}
+
+.infoBox {
+  font-size: 0.5em;
+  color: grey;
+}
+
+.info {
+  color: white;
+}
+
+.logo {
+  border-radius: 50%;
+  border: 1px solid #f8971d;
+}
+</style>
