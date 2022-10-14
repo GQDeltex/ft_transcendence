3950fc88566fbd1a6cefeca65b84260c6e21e930
c8p2d01 <c8p2d01@gmail.com>
2022-10-15 00:14:13 +0200

    titus rules the titles

--- backend/src/auth/strategy/intra42.strategy.ts
+++ backend/src/auth/strategy/intra42.strategy.ts
@@ -7,6 +7,7 @@ import { CreateUserInput } from '../../users/dto/create-user.input';
 /* eslint-disable */
 // @ts-ignore
 import { Strategy, VerifyCallBack } from 'passport-42';
+import { InputTypeFactory } from '@nestjs/graphql/dist/schema-builder/factories/input-type.factory';
 
 @Injectable()
 export class Intra42Strategy extends PassportStrategy(Strategy, 'intra42') {
@@ -34,15 +35,18 @@ export class Intra42Strategy extends PassportStrategy(Strategy, 'intra42') {
       picture: profile._json.image_url,
       campus: profile._json.campus[0].name,
       country: profile._json.campus[0].country,
-      title: clean_42_title(profile._json.titles),
+      title: clean_42_title(
+        profile._json.titles,
+        titusPullus(profile._json.titles_users),
+      ),
     };
     done(null, user);
   }
 }
 
-function clean_42_title(input: { id: number; name: string }[]) {
+function clean_42_title(input: { id: number; name: string }[], index : number) {
   var tmp: string;
-  var output: string[] = [];
+  var output: string[] = [''];
 
   input.forEach((item: { id: number; name: string }) => {
     tmp = item.name;
@@ -51,5 +55,17 @@ function clean_42_title(input: { id: number; name: string }[]) {
     tmp = tmp.trim();
     output.push(tmp);
   });
+  if (index >= 0)
+    output[0] = output[index + 1];
   return output;
+}
+
+function titusPullus(input: { id : number; user_id : number; title_id : number; selected : boolean; created_at : string; updated_at : string}[]) {
+  let i :number = 0;
+  while ( i < input.length) {
+    if (input[i].selected === true)
+      return (i);
+    i++;
+  }
+  return (-1);
 }
