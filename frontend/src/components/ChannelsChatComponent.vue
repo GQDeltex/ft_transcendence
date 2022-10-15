282c61832a8e9af741d7432e827582340a1d443a
René Kaufmann <104884267+Kromolux@users.noreply.github.com>
2022-10-15 21:20:46 +0200

    68 chat friendspeople component (#133)
    * implemented multiple chat components
    
    * changed chat components added chatClientComponent
    
    * implemented users in database with status color

---
+++ frontend/src/components/ChannelsChatComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="channelsParent">
+    <span class="text">Channels</span>
+    <div class="list"></div>
+  </div>
+</template>
+
+<style scoped>
+.channelsParent {
+  display: grid;
+  padding: 0.5vw;
+  border: 1px solid #202020;
+}
+.text {
+  font-size: 1vw;
+  color: #f8971d;
+}
+.list {
+  max-height: 28vh;
+  overflow-y: scroll;
+}
+</style>
---
+++ frontend/src/components/RequestsChatComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="requestsParent">
+    <span class="text">Requests</span>
+    <div class="list"></div>
+  </div>
+</template>
+
+<style scoped>
+.requestsParent {
+  display: grid;
+  padding: 0.5vw;
+  border: 1px solid #202020;
+}
+.text {
+  font-size: 1vw;
+  color: #f8971d;
+}
+.list {
+  max-height: 25vh;
+  overflow-y: scroll;
+}
+</style>
