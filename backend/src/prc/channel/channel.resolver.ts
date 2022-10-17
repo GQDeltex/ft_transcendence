242baa1de43406ceb83b25d1d53d7184a97daaef
Vincent Heymans <vheymans@c2s17d101.42wolfsburg.de>
2022-10-17 18:58:31 +0200

    Fixed Absolute paths

--- backend/src/prc/channel/channel.resolver.ts
+++ backend/src/prc/channel/channel.resolver.ts
@@ -1,6 +1,6 @@
 import { UseGuards } from '@nestjs/common';
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { GqlJwtAuthGuard } from 'src/auth/guard/jwt.guard';
+import { GqlJwtAuthGuard } from '../../auth/guard/jwt.guard';
 import { ChannelService } from './channel.service';
 import { CreateChannelInput } from './dto/create-channel.input';
 import { Channel } from './entities/channel.entity';
