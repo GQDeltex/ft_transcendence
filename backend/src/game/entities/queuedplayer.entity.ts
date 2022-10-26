9cc8ad12579fe5b68cf3324cfdf64ecf74e12eb2
Peter Stengl <pstengl@student.42wolfsburg.de>
2022-10-26 14:47:03 +0200

    Remove the GraphQL fields from the QueuedPlayer

--- backend/src/game/entities/queuedplayer.entity.ts
+++ backend/src/game/entities/queuedplayer.entity.ts
@@ -1,15 +1,11 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
 
-@ObjectType()
 @Entity()
 export class QueuedPlayer {
   @PrimaryGeneratedColumn({ type: 'int' })
-  @Field(() => Int)
   id: number;
 
-  @Field(() => User)
   @ManyToOne(() => User, { nullable: false })
   player1: User;
 }
