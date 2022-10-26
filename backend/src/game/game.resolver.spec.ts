4794bf6c4c89766d9c0a665b6129db8e3d9ec7f5
Peter Stengl <pstengl@student.42wolfsburg.de>
2022-10-26 14:46:47 +0200

    Fix tests (not add any just make the toBeDefined work)

--- backend/src/game/game.resolver.spec.ts
+++ backend/src/game/game.resolver.spec.ts
@@ -1,16 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameResolver } from './game.resolver';
 import { GameService } from './game.service';
+import { Game } from './entities/game.entity';
+import { MockRepo } from '../tools/memdb.mock';
 
 describe('GameResolver', () => {
   let resolver: GameResolver;
+  let mockRepoGame: MockRepo;
 
   beforeEach(async () => {
+    mockRepoGame = new MockRepo('GameResolver', Game);
+    await mockRepoGame.setupDb();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GameResolver, GameService],
+      providers: [GameResolver, GameService, mockRepoGame.getProvider()],
     }).compile();
 
     resolver = module.get<GameResolver>(GameResolver);
+  });
+
+  afterEach(async () => {
+    await mockRepoGame.clearRepo();
+  });
+
+  afterAll(async () => {
+    await mockRepoGame.destroyRepo();
   });
 
   it('should be defined', () => {
--- backend/src/game/game.service.spec.ts
+++ backend/src/game/game.service.spec.ts
@@ -1,15 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameService } from './game.service';
+import { Game } from './entities/game.entity';
+import { MockRepo } from '../tools/memdb.mock';
 
 describe('GameService', () => {
   let service: GameService;
+  let mockRepoGame: MockRepo;
 
   beforeEach(async () => {
+    mockRepoGame = new MockRepo('GameService', Game);
+    await mockRepoGame.setupDb();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GameService],
+      providers: [GameService, mockRepoGame.getProvider()],
     }).compile();
 
     service = module.get<GameService>(GameService);
+  });
+
+  afterEach(async () => {
+    await mockRepoGame.clearRepo();
+  });
+
+  afterAll(async () => {
+    await mockRepoGame.destroyRepo();
   });
 
   it('should be defined', () => {
