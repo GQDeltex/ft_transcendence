e7da25ffd5426ee1b2560de6e5c6845eeb2cd902
Hendrik Lehmann <hendrik.lehmann96@gmail.com>
2022-10-27 20:41:00 +0200

    adding queue view

---
+++ frontend/src/views/QueueView.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="parent">
+    <p class="saving">In Queue<span>.</span><span>.</span><span>.</span></p>
+    <div class="loader"></div>
+  </div>
+</template>
+
+<style scoped>
+.parent {
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.loader {
+  border: 16px solid #f8971d;
+  border-top: 16px solid gray;
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes blink {
+  0% {
+    opacity: 0.2;
+  }
+  20% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0.2;
+  }
+}
+
+.saving {
+  font-size: 5em;
+  margin-bottom: 0.3em;
+}
+
+.saving span {
+  animation-name: blink;
+  animation-duration: 1.4s;
+  animation-iteration-count: infinite;
+  animation-fill-mode: both;
+}
+
+.saving span:nth-child(2) {
+  animation-delay: 0.2s;
+}
+
+.saving span:nth-child(3) {
+  animation-delay: 0.4s;
+}
+</style>
