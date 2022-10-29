c1258cf71f9de988d0b8a279c13a883b649661fd
Clemens Dahlhoff <78210806+c8p2d01@users.noreply.github.com>
2022-10-29 15:04:44 +0200

    266 game flow (#300)
    * add queue in game page
    
    * gameGate(game:Game):GameGate
    
    * first paddle sync, ball differs
    
    * balls moving in sync
    
    * (time)limit less gaming
    
    * game Ended
    
    * Update Documentation
    
    * Fix test for game.service.ts
    
    * Fix the other tests too
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: GQDeltex <pstengl@student.42wolfsburg.de>

---
+++ frontend/src/components/game/QueueComponent.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { socket } from '../../service/socket';
+import PongComponent from './PongComponent.vue';
+
+const displayState = ref(true);
+const gameIdRef = ref(0);
+const playerPriorityRef = ref(false);
+
+function join_queue() {
+  socket.emit('queue', { event: 'JOIN' });
+}
+function leave_queue() {
+  socket.emit('queue', { event: 'LEAVE' });
+}
+
+// playerIDs to check validity of messages for streaming implementation later™
+socket.on('Game', ({ gameId, player1Id, player2Id, priority }) => {
+  if (gameId < 0) {
+    displayState.value = true;
+    return;
+  }
+  displayState.value = false;
+  console.log('ich will ein spiel mit dir spielen');
+  gameIdRef.value = gameId;
+  playerPriorityRef.value = priority;
+});
+</script>
+
+<template>
+  <div v-if="displayState">
+    <button @click="join_queue">JOIN Queue</button>
+    <button @click="leave_queue">leave Queue</button>
+  </div>
+  <PongComponent v-else :game-id="gameIdRef" :priority="playerPriorityRef" />
+</template>
