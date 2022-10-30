029a703247dd4d19a53bf931feb6adb2f95b5ee5
eduard hosu <ehosu@student.42wolfsburg.de>
2022-10-30 19:07:42 +0100

    added a new endscreen component and a ts file, work in progress.
    Co-authored-by: Dejan Zivanov <77687815+dejanzivanov@users.noreply.github.com>

---
+++ frontend/src/components/game/EndScreenComponent.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import Queue from './queue';
+
+// defineProps <{
+// 	playerScore: number;
+// 	remoteScore: 
+// }
+
+function homepage() {
+	window.location.href = '/';
+}
+
+</script>
+
+<template>
+  <div class="parent">
+    <p class="saving">Some player wins!</p>
+	<button class="home-page" @click="homepage()">Home page</button>
+	<button class="re-queue" @click="Queue.join_queue()">Re-Queue</button>
+	<p class="saving">Player Score: {{ Queue.playerScore }}</p>
+	<p class="saving">Player Remote: {{ Queue.remoteScore }}</p>
+    <div class="loader"></div>
+  </div>
+</template>
+
+<style scoped>
+.parent {
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+</style>
--- frontend/src/components/game/QueueComponent.vue
+++ frontend/src/components/game/QueueComponent.vue
@@ -2,14 +2,15 @@
 import { ref } from 'vue';
 import { socket } from '../../service/socket';
 import PongComponent from './PongComponent.vue';
+import EndScreenComponent from './EndScreenComponent.vue';
+import Queue from './queue';
 
-const displayState = ref(true);
+
+// const displayState = ref(true);
+const displayState = ref('queue');
 const gameIdRef = ref(0);
 const playerPriorityRef = ref(false);
 
-function join_queue() {
-  socket.emit('queue', { event: 'JOIN' });
-}
 function leave_queue() {
   socket.emit('queue', { event: 'LEAVE' });
 }
@@ -17,20 +18,27 @@ function leave_queue() {
 // playerIDs to check validity of messages for streaming implementation later™
 socket.on('Game', ({ gameId, player1Id, player2Id, priority }) => {
   if (gameId < 0) {
-    displayState.value = true;
+    // displayState.value = true;
+    displayState.value = 'end';
     return;
   }
-  displayState.value = false;
+//   displayState.value = false;
+  displayState.value = 'start';
   console.log('ich will ein spiel mit dir spielen');
   gameIdRef.value = gameId;
   playerPriorityRef.value = priority;
 });
+
 </script>
 
 <template>
-  <div v-if="displayState">
-    <button @click="join_queue">JOIN Queue</button>
+  <!-- <div v-if="displayState"> -->
+  <div v-if="displayState === 'queue'">
+    <button @click="Queue.join_queue()">JOIN Queue</button>
     <button @click="leave_queue">leave Queue</button>
   </div>
+  <!-- <EndScreenComponent v-else-if :game-id="gameIdRef" :priority="playerPriorityRef" /> -->
+  <!-- <PongComponent v-else :game-id="gameIdRef" :priority="playerPriorityRef" /> -->
+  <EndScreenComponent v-else-if="displayState === 'end'" :game-id="gameIdRef" :priority="playerPriorityRef" />
   <PongComponent v-else :game-id="gameIdRef" :priority="playerPriorityRef" />
 </template>
