c9a274f3da99a85e245a1e53186965133ea344b6
Hendrik Lehmann <hendrik.lehmann96@gmail.com>
2022-10-31 11:06:23 +0100

    prettifying the history

--- frontend/src/components/profile/history/ParentHistoryComponent.vue
+++ frontend/src/components/profile/history/ParentHistoryComponent.vue
@@ -20,22 +20,33 @@ if (typeof user !== 'undefined' && typeof user.user !== 'undefined') {
 
 <template>
   <div class="frame">
-    <UserPlayedGameComponent
-      v-for="game in games"
-      :key="game.id"
-      :player1="game.player1.username"
-      :player2="game.player2.username"
-      :score1="game.score1"
-      :score2="game.score2"
-    />
+    <div class="title">History</div>
+    <div class="scroll">
+      <UserPlayedGameComponent
+        v-for="game in games"
+        :key="game.id"
+        :player1="game.player1.username"
+        :player2="game.player2.username"
+        :score1="game.score1"
+        :score2="game.score2"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped>
+.title {
+  font-size: 2vw;
+}
+
+.scroll {
+  overflow-y: scroll;
+  max-height: inherit;
+}
 .frame {
   border: 1px solid #202020;
   padding: 1vw;
   max-height: inherit;
-  overflow-y: scroll;
+  /* overflow-y: scroll; */
 }
 </style>
