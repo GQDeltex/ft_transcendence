19e30be7c8124697877154430f710f5204af4998
René Kaufmann <104884267+Kromolux@users.noreply.github.com>
2022-11-08 14:09:44 +0100

    448 change order of games on profile page (#456)
    * frontend sorting implemented - WIP lodash error
    
    * updated npm package

--- frontend/src/components/profile/history/ParentHistoryComponent.vue
+++ frontend/src/components/profile/history/ParentHistoryComponent.vue
@@ -5,15 +5,20 @@ import type { Game } from '@/service/GameService';
 import type { User } from '@/store/user';
 import type { Ref } from 'vue';
 import { ref, inject, watchEffect } from 'vue';
+import { cloneDeep } from 'lodash';
 
 const user = inject<{ user: Ref<User>; isMe: Ref<boolean> }>('user');
 const games = ref<Game[]>([]);
+const sortedGames = ref<Game[]>([]);
 
 if (typeof user !== 'undefined' && typeof user.user !== 'undefined') {
   watchEffect(async () => {
     GameService.findAll('ended', user.user.value.id).then(
       (gamesreturn: Game[]) => (games.value = gamesreturn),
     );
+    sortedGames.value = cloneDeep(games.value);
+    sortedGames.value.sort((a, b) => b.id - a.id);
+    // console.log(sortedGames.value);
   });
 }
 </script>
@@ -23,7 +28,7 @@ if (typeof user !== 'undefined' && typeof user.user !== 'undefined') {
     <div class="title">History</div>
     <div class="scroll">
       <UserPlayedGameComponent
-        v-for="game in games"
+        v-for="game in sortedGames"
         :key="game.id"
         :player1="game.player1.username"
         :player2="game.player2.username"
